f207d012145f996a653e52dd1deab921fb433a7d
Gixya <giovannafgoncalves10@gmail.com>
2026-04-23 17:35:09 -0300

    Add files via upload

---
+++ threads-secao-critica/ThreadsComTrava/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+
+class Program
+{
+    static int numThreads = 20;
+    static int linhasPorThread = 500;
+    static string caminhoArquivo = "com_trava.txt";
+
+    static readonly object trava = new object();
+
+    static void Main()
+    {
+        Thread[] threads = new Thread[numThreads];
+
+        File.WriteAllText(caminhoArquivo, "");
+
+        for (int i = 0; i < numThreads; i++)
+        {
+            int idThread = i;
+            threads[i] = new Thread(() => EscreverComTrava(idThread));
+            threads[i].Start();
+        }
+
+        foreach (Thread t in threads)
+            t.Join();
+
+        Console.WriteLine("Finalizado COM seção crítica!");
+    }
+
+    static void EscreverComTrava(int id)
+    {
+        for (int i = 0; i < linhasPorThread; i++)
+        {
+            string linha = $"Thread {id} - Linha {i}\n";
+
+            lock (trava)
+            {
+                File.AppendAllText(caminhoArquivo, linha);
+            }
+
+            Thread.Sleep(1);
+        }
+    }
+}
---
+++ threads-secao-critica/ThreadsSemTrava/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+
+class Program
+{
+    static int numThreads = 20;
+    static int linhasPorThread = 500;
+    static string caminhoArquivo = "sem_trava.txt";
+
+    static void Main()
+    {
+        Thread[] threads = new Thread[numThreads];
+
+        File.WriteAllText(caminhoArquivo, "");
+
+        for (int i = 0; i < numThreads; i++)
+        {
+            int idThread = i;
+            threads[i] = new Thread(() => EscreverSemTrava(idThread));
+            threads[i].Start();
+        }
+
+        foreach (Thread t in threads)
+            t.Join();
+
+        Console.WriteLine("Finalizado SEM seção crítica!");
+    }
+
+    static void EscreverSemTrava(int id)
+    {
+        for (int i = 0; i < linhasPorThread; i++)
+        {
+            string linha = $"Thread {id} - Linha {i}\n";
+            File.AppendAllText(caminhoArquivo, linha);
+            Thread.Sleep(1);
+        }
+    }
+}
